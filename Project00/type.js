6309c7a0f782b29f3c52316633063bd8466e6084
smasnb12 <smasnb12@gmail.com>
2022-12-12 18:06:59 +0500

    update

---
+++ Project00/type.js
@@ -0,0 +1,8 @@
+// const firstName = "Abbas"
+// const nameLength = firstName.length()
+
+function sayMyName (fullname){
+    console.log(`My full name is ${fullname}`);
+    
+}
+sayMyName("Abbas", "Shah")
---
+++ union.ts
@@ -0,0 +1,18 @@
+let myAge : string | number
+
+myAge = 45
+
+myAge = "don't know"
+console.log(myAge.toString());
+
+let newAge = Math.random() > 0.5 ? "Abbas" : 45
+if (newAge == "Abbas"){
+    console.log(newAge.toUpperCase);
+    
+}else{
+    console.log(newAge);
+    
+}
+if (typeof newAge === "string"){
+    
+}
