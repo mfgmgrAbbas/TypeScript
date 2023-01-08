2fbc7a35287f10e52b858494632a5662af658e6a
smasnb12 <smasnb12@gmail.com>
2023-01-08 19:04:26 +0500

    analytic

--- Next.js/hello_world/pages/_app.js
+++ Next.js/hello_world/pages/_app.js
@@ -1,10 +1,12 @@
 import Layout from '../comps/Layout'
 import '../styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
         <Component {...pageProps} />
+        <Analytics />
 
       </Layout> 
   )
