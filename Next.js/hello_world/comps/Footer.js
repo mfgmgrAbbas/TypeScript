fdfde17942670b69f0a27fb1cb1bf7f8b992ece2
smasnb12 <smasnb12@gmail.com>
2023-01-08 14:35:55 +0500

    remove

--- Next.js/hello_world/comps/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return ( 
-        <footer>
-            Copyright 2023 Developers Hub
-        </footer>
-     );
-}
- 
-export default Footer;
--- Next.js/hello_world/pages/blog/[id].js
+++
@@ -1,9 +0,0 @@
-const Details = () => {
-    return ( 
-        <div>
-            <h1>Details</h1>
-        </div>
-     );
-}
- 
-export default Details;
